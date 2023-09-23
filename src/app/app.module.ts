faae8c516b0ab329a28ec6a9e73fd5a8531aa8a1
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2023-09-23 15:28:31 +0200

    chore: cleanup unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SwitchableViewComponent } from './switchable-view/switchable-view.component';
-import { CharacterClassComponent } from './switchable-view/character-class/character-class.component';
-import { ItemFeatureComponent } from './switchable-view/item-feature/item-feature.component';
 import { FactoryViewModule } from './factory-view/factory-view.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwitchableViewComponent,
-    CharacterClassComponent,
-    ItemFeatureComponent
   ],
   imports: [
     BrowserModule,
